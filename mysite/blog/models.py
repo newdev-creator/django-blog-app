91e78cd23679e17ab3467d5a2824983e751e1e8a
Julien <newdevcreator@gmail.com>
2024-11-05 23:51:26 +0100

    FEAT: add blog to installed_apps and add Status class in blog/models

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 from django.utils import timezone
-from sympy import Indexed
 
 
 class Blog(models.Model):
+    
+    class Status(models.IntegerChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status,
+        default=Status.DRAFT,
+    )
 
     class Meta:
         ordering = ('-publish',)
