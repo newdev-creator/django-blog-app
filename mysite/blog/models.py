50663fd970a6c6778945bc149eec3974009dd2db
Julien <newdevcreator@gmail.com>
2024-11-08 23:34:04 +0100

    FEAT: Comment

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,4 @@
+from operator import index
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
@@ -59,3 +60,26 @@ class Post(models.Model):
                 self.slug,
             ],
         )
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    activate = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
