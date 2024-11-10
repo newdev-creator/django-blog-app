8519e80fd84c47f6219debc308d973eb707b2b65
Julien <newdevcreator@gmail.com>
2024-11-10 17:31:55 +0100

    FEAT: markdown

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'django.contrib.staticfiles',
 
     # Third-party apps
