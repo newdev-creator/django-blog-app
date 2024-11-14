365aa88c88dd1d6bb4501c8d25af14059208ad78
Julien <newdevcreator@gmail.com>
2024-11-14 21:51:53 +0100

    FEAT: update vector

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,7 @@ from re import search
 from django.core.mail import send_mail
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import (SearchVector, SearchQuery, SearchRank)
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 from django.db.models import Count
@@ -170,11 +170,17 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector(
+                'title', weight='A'
+                ) + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=SearchVector('title', 'body')
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
                 )
-                .filter(search=query)
+                .filter(rank__gte=0.3)
+                .order_by('-rank')
             )
 
     return render(
